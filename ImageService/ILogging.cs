116bb621c8b13c409d226fe256fd381253d6e547
JonathanBerger1 <jbdeco20@gmail.com>
2018-04-15 20:19:37 +0300

    committing the finished project

---
+++ ImageService/ILogging.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ILogging
+{
+    public interface ILogging
+    {
+        event EventHandler<MessageRecievedEventArgs> MessageRecieved;
+        void Log(string message, MessageTypeEnum type);           // Logging the Message
+    }
+}
+
---
+++ ImageService/LoggingModal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class LoggingModal : ILogging
+{
+    public event EventHandler<MessageRecievedEventArgs> MessageRecieved;
+    public void Log(string message, MessageTypeEnum type)
+    {
+        MessageRecieved.Invoke(this, new MessageRecievedEventArgs(message, type));
+    }
+}
